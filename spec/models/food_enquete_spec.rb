759fe9e2770f848cc5c4837794e44804fe40d7ab
ookidoshi <okidookido@gmail.com>
2020-10-29 12:10:54 +0900

    RSpec
    Only adults can apply

--- spec/models/food_enquete_spec.rb
+++ spec/models/food_enquete_spec.rb
@@ -73,6 +73,41 @@ RSpec.describe FoodEnquete, type: :model do
       end
     end
   end
+describe 'アンケート回答時の条件' do
+  context '年齢を確認すること' do
+    it '未成年はビール飲み放題を選択できないこと' do
+      #未成年のテストデータを作成します
+      enquete_sato = FoodEnquete.new(
+        name: '佐藤 仁美',
+        mail: 'hitomi.sato@example.com',
+        age: 19,
+        food_id: 2,
+        score: 3,
+        request: 'おいしかったです。',
+        present_id: 1   # ビール飲み放題
+      )
+      expect(enquete_sato).not_to be_valid
+      #成人のみ選択できる旨のメッセージが含まれることを検証します
+      expect(enquete_sato.errors[:present_id]).to include(I18n.t('activerecord.errors.models.food_enquete.attributes.present_id.cannot_present_to_minor'))
+    end
+
+      it '成人はビール飲み放題を選択できないこと' do
+        #未成年のテストデータを作成します
+            enquete_sato = FoodEnquete.new(
+              name: '佐藤 仁美',
+              mail: 'hitomi.sato@example.com',
+              age: 20,
+              food_id: 2,
+              score: 3,
+              request: 'おいしかったです。',
+              present_id: 1   # ビール飲み放題
+            )
+      #バリデーションが正常に通ること（バリデーションエラーがないこと）を検証します
+      expect(enquete_sato).to be_valid
+    end
+  end
+end
+
     describe '#adult?' do
       it '20歳未満は成人でないこと' do
         foodEnquete = FoodEnquete.new
